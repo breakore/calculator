d7096c8f3c3ab8d01be8c58efd629e9a8d7096c8
breakore <breakore@gmail.com>
2022-01-09 09:24:27 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,84 @@
+let a = '';  //first num
+let b = ''; //sec num
+let sign = ''; //sign
+let finish = false;
+
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const action = ['-', '+', 'X', '/'];
+
+//screen
+const out = document.querySelector('.calc-screen p');
+
+function clearAll() {
+  a = '';// first num and result
+  b = ''; //second num
+  sign = ''; //sign
+  finish = false;
+  out.textContent = 0;
+}
+document.querySelector('.ac').onclick = clearAll;
+
+document.querySelector('.buttons').onclick = (event) => {
+  //нажата не кнопка
+  if (!event.target.classList.contains('btn')) return;
+  //нажата кнопка ac
+  if (event.target.classList.contains('ac')) return;
+
+  out.textContent = '';
+  // получаю нажатую кнопку
+  const key = event.target.textContent;
+
+  //если нажата кнопка 0-9 или .
+  if (digit.includes(key)) {
+    if (b === '' && sign === '') {
+      a += key;
+      console.log(a, b, sign);
+      out.textContent = a;
+    }
+    else if (a !== '' && b !== '' && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    }
+
+    else {
+      b += key;
+      out.textContent = b;
+    }
+  }
+  //если нажата клавиша операции
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    console.log(a, b, sign);
+    return;
+  }
+
+  //нажата равно
+  if (key === '=') {
+    if (b === '') b = a;
+    switch (sign) {
+      case "+":
+        a = (+a) + (+b);
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "X":
+        a = a * b;
+        break;
+      case "/":
+        if (b === '0') {
+          out.textContent = 'Ошибка';
+          a = '';
+          b = '';
+          sign = '';
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+    console.log(a, b, sign);
+  }
